bfe3f68b6caaae1db3babfa19eba7f89796c5100
Neha Anand <neha8anand@gmail.com>
2018-08-18 22:49:42 -0700

    Add prof_ratings scraper

--- data/prof_ratings.py
+++ data/prof_ratings.py
@@ -5,3 +5,49 @@ import json
 # school ids for lookup on ratemyprofessors.com
 with open('sids.json', 'r') as sids:
     sids = json.load(sids)
+
+# school and faculty names for lookup on ratemyprofessors.com
+with open('utpge.json', 'r') as f:
+    ut = json.load(f)
+
+# university rankings and other info pulled from usnews website
+with open('university_rankings.json', 'r') as f:
+    university_rankings = json.load(f)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+
+def rating_and_tags(sid, school_name, faculty_name):
+    '''Gives rating and tags for a professor pulled from ratemyprofessors.com
+       given sid (school's id as per ratemyprofessors.com), school_name and faculty_name
+    '''
+
+     # Getting rating url by searching school and faculty name
+    url = f"http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName={school_name}&schoolID={sid}&query={faculty_name}"
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    try:
+        rating_url = "http://www.ratemyprofessors.com" + soup.select("li.listing.PROFESSOR")[0].find("a")["href"]
+
+        # Getting rating and profile tags for the rating url
+        response2 = requests.get(rating_url, headers=headers)
+        soup2 = BeautifulSoup(response2.content, 'html.parser')
+        rating = float(soup2.select("div.grade")[0].get_text())
+        tags = [tag_desc.get_text() for tag_desc in soup2.select("span.tag-box-choosetags")]
+
+    except:
+        rating, tags = 'N/A', 'N/A'
+
+    return {'rating': rating, 'tags': tags}
+
+# Creating complete university database
+university = university_rankings.copy()
+sid = '1255'
+school = 'University of Texas at Austin'
+
+for faculty in ut['faculty_names']:
+    university['petroleum-engineering']['University of Texas--Austin (Cockrell)'] [faculty] = rating_and_tags(sid, school, faculty)
+
+# Writing results to a json file
+with open('university.json', 'w') as f:
+    json.dump(university, f)
---
+++ data/ut_pge.py
@@ -0,0 +1,32 @@
+import requests
+import bs4
+import json
+from bs4 import BeautifulSoup
+
+ut_pge_url = "https://www.pge.utexas.edu"
+ut_pge_faculties_page_url = ut_pge_url + "/facultystaff/profiles"
+ut_pge_faculties_page = requests.get(ut_pge_faculties_page_url, verify=False)
+ut_pge_faculties_soup = BeautifulSoup(ut_pge_faculties_page.content, 'html.parser')
+
+ut_pge_faculty_entries = ut_pge_faculties_soup.find_all(class_="facentry")
+
+faculty_names_dictionary = {}
+faculty_names_dictionary['university_name'] = "University of Texas"
+faculty_names_dictionary['faculty_names'] = []
+
+for ut_pge_faculty_entry in ut_pge_faculty_entries:
+        ut_pge_faculty_info = list(ut_pge_faculty_entry.find_all("td"))
+        ut_pge_faculty_link = ut_pge_faculty_info[0].find("a")['href']
+
+        ut_pge_faculty_page_url = ut_pge_url + ut_pge_faculty_link
+        ut_pge_faculty_page = requests.get(ut_pge_faculty_page_url, verify=False)
+
+        ut_pge_faculty_soup = BeautifulSoup(ut_pge_faculty_page.content, 'html.parser')
+        ut_pge_faculty_name = ut_pge_faculty_soup.select("div#mainbody2")[0].find_all("h1")[0].get_text()
+
+        faculty_name = ut_pge_faculty_name.replace("  ", " ") #Name in HTML contains 2 spaces instead of 1
+        faculty_names_dictionary['faculty_names'].append(faculty_name)
+
+with open('faculty_names/ut_pge_faculty_names.json', 'w') as ut_pge_faculty_names_file:
+    json.dump(faculty_names_dictionary, ut_pge_faculty_names_file)
+    
