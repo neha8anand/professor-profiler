95aec57389c31dafc56374d57a2cada9716bda25
Neha Anand <neha8anand@gmail.com>
2018-08-18 23:22:19 -0700

    Edit ut_pge scraper

--- data/ut_pge.py
+++ data/ut_pge.py
@@ -5,29 +5,29 @@ from bs4 import BeautifulSoup
 import warnings
 warnings.filterwarnings("ignore") # ut's certificate is not trusted
 
-ut_pge_url = "https://www.pge.utexas.edu"
-ut_pge_faculties_page_url = ut_pge_url + "/facultystaff/profiles"
-ut_pge_faculties_page = requests.get(ut_pge_faculties_page_url, verify=False)
-ut_pge_faculties_soup = BeautifulSoup(ut_pge_faculties_page.content, 'html.parser')
+pge_url = "https://www.pge.utexas.edu"
+pge_faculties_page_url = pge_url + "/facultystaff/profiles"
+pge_faculties_page = requests.get(pge_faculties_page_url, verify=False)
+pge_faculties_soup = BeautifulSoup(pge_faculties_page.content, 'html.parser')
 
-ut_pge_faculty_entries = ut_pge_faculties_soup.find_all(class_="facentry")
+pge_faculty_entries = pge_faculties_soup.find_all(class_="facentry")
 
 faculty_names_dictionary = {}
 faculty_names_dictionary['university_name'] = "University of Texas at Austin"
 faculty_names_dictionary['faculty_names'] = []
 
-for ut_pge_faculty_entry in ut_pge_faculty_entries:
-        ut_pge_faculty_info = list(ut_pge_faculty_entry.find_all("td"))
+for pge_faculty_entry in pge_faculty_entries:
+        pge_faculty_info = list(pge_faculty_entry.find_all("td"))
         # getting the faculty page url
-        ut_pge_faculty_link = ut_pge_faculty_info[0].find("a")['href']
+        pge_faculty_link = pge_faculty_info[0].find("a")['href']
 
-        ut_pge_faculty_page_url = ut_pge_url + ut_pge_faculty_link
-        ut_pge_faculty_page = requests.get(ut_pge_faculty_page_url, verify=False)
+        pge_faculty_page_url = pge_url + pge_faculty_link
+        pge_faculty_page = requests.get(pge_faculty_page_url, verify=False)
 
-        ut_pge_faculty_soup = BeautifulSoup(ut_pge_faculty_page.content, 'html.parser')
-        ut_pge_faculty_name = ut_pge_faculty_soup.select("div#mainbody2")[0].find_all("h1")[0].get_text()
+        pge_faculty_soup = BeautifulSoup(pge_faculty_page.content, 'html.parser')
+        pge_faculty_name = pge_faculty_soup.select("div#mainbody2")[0].find_all("h1")[0].get_text()
 
-        faculty_name = ut_pge_faculty_name.replace("  ", " ") # Name in HTML contains 2 spaces instead of 1
+        faculty_name = pge_faculty_name.replace("  ", " ") # Name in HTML contains 2 spaces instead of 1
         faculty_names_dictionary['faculty_names'].append(faculty_name)
 
 with open('ut_pge.json', 'w') as ut_pge_faculty_names_file:
