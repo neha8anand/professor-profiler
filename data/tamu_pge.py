58b8e3317b6d42ffa234c076f44ee3f88fe03c05
Neha Anand <neha8anand@gmail.com>
2018-08-19 11:27:12 -0700

    Add completed tamu_pge scraper

--- data/tamu_pge.py
+++ data/tamu_pge.py
@@ -2,25 +2,43 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-faculties_page_url = "https://engineering.tamu.edu/profile-data.json"
+faculties_page_url = 'https://engineering.tamu.edu/profile-data.json'
 faculties_page = requests.get(faculties_page_url)
 
 faculty_names_dictionary = {}
 
-for faculty_page in faculties_page.json():
-    if 'petroleum' in faculty_page['tag']:
-        faculty = faculty_page['name']
-        link = "https://engineering.tamu.edu" + faculty_page['link']
-        phone = faculty_page['phone']
-        office = faculty_page['office']
-        email = faculty_page['email']
-        title = faculty_page['titles'][0]
+for faculty_entry in faculties_page.json():
+    if 'petroleum' in faculty_entry['tag']:
+        faculty = faculty_entry['name']
+        title = faculty_entry['titles'][0]
+        page = 'https://engineering.tamu.edu' + faculty_entry['link']
+        phone = faculty_entry['phone']
+        office = faculty_entry['office']
+        email = faculty_entry['email']
+        research_areas = '' # for faculties whose research_areas are not defined
+
+        # google-scholar profile link
+        faculty_page = requests.get(page)
+        soup = BeautifulSoup(faculty_page.content, 'html.parser')
+        for a_tag in soup.select('a.button '):
+            if 'scholar.google.com' in a_tag['href']:
+                google_scholar_link = a_tag['href']
+                break
+
+        # research areas
+        for div_tag in soup.select('div.simple-list-collection__list'):
+            if 'Research Interests' in div_tag.get_text():
+                research_areas = div_tag.select("ul.no-bullets")[0].get_text().strip('\n').strip('\xa0').split('\n')
+                research_areas = [area for area in research_areas if area]
+                break
 
         faculty_names_dictionary[faculty] = {'title' : title,
-                                            'link' : link,
+                                             'page' : page,
                                              'phone' : phone,
-                                            'office': office,
-                                            'email': email}
+                                             'office': office,
+                                             'email': email,
+                                             'research_areas' : research_areas,
+                                             'google_scholar_link' : google_scholar_link}
 
 pge_dictionary = {}
 pge_dictionary['university_name'] = 'Texas A&M University--College Station'
