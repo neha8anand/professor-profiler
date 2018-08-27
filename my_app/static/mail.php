d4a19c42d42004b421ba6cdbd31d3d6e8c45310e
Neha Anand <neha8anand@gmail.com>
2018-08-27 16:46:05 -0700

    Update html pages

--- my_app/static/mail.php
+++ my_app/static/mail.php
@@ -1,10 +1,10 @@
 <?php
-    $to = 'demo@spondonit.com';
+    $to = 'neha8anand@gmail.com';
     $firstname = $_POST["fname"];
     $email= $_POST["email"];
     $text= $_POST["message"];
     $phone= $_POST["phone"];
-    
+
 
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
@@ -18,7 +18,7 @@
         <tr><td>Email: '.$email.'</td></tr>
         <tr><td>phone: '.$phone.'</td></tr>
         <tr><td>Text: '.$text.'</td></tr>
-        
+
     </table>';
 
     if (@mail($to, $email, $message, $headers))
